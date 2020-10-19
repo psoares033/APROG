4dcd4ffa2defaeaef9c53c720f0ab4abb6126407
Hugo Soares <worriedhugo@gmail.com>
2020-10-19 23:35:11 +0100

    Projeto 1
    Este é o meu primeiro commit

---
+++ tp01_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+
+int main(int argc, char const *argv[])
+{
+    int p, n;
+
+    printf ("Introduza o valor que pretende triplicar: ");
+    scanf("%d" , &n);
+    p = n * k;
+    printf ("O triplo de %d é %d", n, p);
+
+    return 0;
+}
